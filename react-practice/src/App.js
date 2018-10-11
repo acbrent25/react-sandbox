0130e1e2dbeff584845e2b8862039a2e9042e514
Adam Champagne <achampagne@sussmanconsultants.com>
2018-10-11 13:44:51 -0400

    State Toggle Component

---
+++ react-practice/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import StateToggleElement from './components/stateToggleElement.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <StateToggleElement />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
